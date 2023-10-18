b13a32ba208ecdbf625916797ec555c35f1e32f3
lucent1090 <lucent1090@gmail.com>
2023-10-18 19:49:26 +0800

    feature: add 1 on 1 chat

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import OpenAI from "openai";
+
+import "./App.css";
+
+const openai = new OpenAI({
+  apiKey: "OPENAI_KEY",
+});
+
+const initChat: OpenAI.ChatCompletionMessage[] = [
+  {
+    role: "system",
+    content:
+      "You are Marv, a chatbot that reluctantly answers questions with sarcastic responses.",
+  },
+  {
+    role: "user",
+    content: "Hey, who are you?",
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [newline, setNewLine] = useState("");
+  const [history, setHistory] =
+    useState<OpenAI.ChatCompletionMessage[]>(initChat);
+
+  const onClick = async () => {
+    const newHistory = history.concat([{ role: "user", content: newline }]);
+    setNewLine("");
+    setHistory(newHistory);
+
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: newHistory,
+      temperature: 0.5,
+      max_tokens: 256,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
+
+    const newMessage = response.choices[0].message;
+    setHistory(newHistory.concat([newMessage]));
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <div className="content">
+        {history
+          .filter((h) => h.role === "user" || h.role === "assistant")
+          .map((line, index) => {
+            return line.role === "assistant" ? (
+              <div key={`${index}${line.role}`} className="bot">
+                Marv <div>{line.content}</div>
+              </div>
+            ) : (
+              <div key={`${index}${line.role}`} className="user">
+                {line.content}
+              </div>
+            );
+          })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className="input-area">
+        <input
+          className="text"
+          value={newline}
+          onChange={(e) => setNewLine(e.target.value)}
+        />
+        <button className="btn" onClick={onClick}>
+          Go
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
